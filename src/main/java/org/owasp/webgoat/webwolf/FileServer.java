97a8c385c69f2e27d6ffe4448185effdb3689d88
IT24100500 <it24100500@my.sliit.lk>
2026-09-22 20:11:11 +0530

    Added Changes for agianst path traversal

--- src/main/java/org/owasp/webgoat/webwolf/FileServer.java
+++ src/main/java/org/owasp/webgoat/webwolf/FileServer.java
@@ -88,7 +88,8 @@ public class FileServer {
     // DO NOT use multipartFile.transferTo(), see
     // https://stackoverflow.com/questions/60336929/java-nio-file-nosuchfileexception-when-file-transferto-is-called
     try (InputStream is = multipartFile.getInputStream()) {
-      var destinationFile = destinationDir.toPath().resolve(multipartFile.getOriginalFilename());
+      var originalFilename = multipartFile.getOriginalFilename();
+      var destinationFile = destinationDir.toPath().resolve(originalFilename).normalize();
       Files.deleteIfExists(destinationFile);
       Files.copy(is, destinationFile);
     }
