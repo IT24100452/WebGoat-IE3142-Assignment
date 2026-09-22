db9aaa77e2bace379b73e4d6a7e9d937361fdfb6
IT24100500 <it24100500@my.sliit.lk>
2026-09-22 20:44:55 +0530

    changing open flaw for file path

--- src/main/java/org/owasp/webgoat/webwolf/FileServer.java
+++ src/main/java/org/owasp/webgoat/webwolf/FileServer.java
@@ -88,7 +88,8 @@ public class FileServer {
     // DO NOT use multipartFile.transferTo(), see
     // https://stackoverflow.com/questions/60336929/java-nio-file-nosuchfileexception-when-file-transferto-is-called
     try (InputStream is = multipartFile.getInputStream()) {
-      var originalFilename = multipartFile.getOriginalFilename(); //Added IT24100500
+      var originalFilename = multipartFile.getOriginalFilename(); //Added IT24100500 for test
+      var destinationFile = new File(destinationDir, originalFilename);
       Files.deleteIfExists(destinationFile);
       Files.copy(is, destinationFile);
     }
